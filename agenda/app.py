8bc3632e0d812c61551b1ddd9d6172d38cccbf3e
Alex-RiP7 <1725110975@utectulancingo.edu.mx>
2026-07-06 14:32:58 +0000

    CREATED

--- agenda/app.py
+++ agenda/app.py
@@ -2,10 +2,13 @@ import web
 
 urls = (
     '/', 'controllers.index.Index',
-    '/lista_contactos','controllers.lista_contactos.ListaContactos',
-    '/ver_contacto/(.+)', 'controllers.ver_contacto.VerContacto'
-    '/editar_contacto/(.+)', 'controllers.editar_contacto.EditarContacto',
+    '/lista_contactos', 'controllers.lista_contactos.ListaContactos',
+    '/ver_contacto/(.+)', 'controllers.ver_contacto.VerContacto',
+    '/editar_contacto/(.+)', 'controllers.editar_contacto.EditarContacto',  # ← Esta es la que faltaba
+    # '/insertar_contacto', 'controllers.insertar_contacto.InsertarContacto',  # descomenta cuando lo tengas
+    # '/borrar_contacto/(.+)', 'controllers.borrar_contacto.BorrarContacto',
 )
+
 app = web.application(urls, globals())
 
 if __name__ == "__main__":
